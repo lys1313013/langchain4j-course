ef1e4379d6eca7a8e6474caf2860d35b645b0f23
lys1313013 <64422807+lys1313013@users.noreply.github.com>
2025-03-12 23:49:34 +0800

    日期改为动态生成

--- langchain4j-13websearch/src/test/java/com/pig4cloud/ai/langchain4j13websearch/config/LLMConfigTest.java
+++ langchain4j-13websearch/src/test/java/com/pig4cloud/ai/langchain4j13websearch/config/LLMConfigTest.java
@@ -5,7 +5,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 @SpringBootTest
 class LLMConfigTest {
@@ -15,7 +16,15 @@ class LLMConfigTest {
 
     @Test
     void chatAssistant() {
-        String chat = chatAssistant.chat("今天20241008 上证指数是多少？");
+        // 获取当前日期
+        LocalDate currentDate = LocalDate.now();
+        // 定义日期格式
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        // 将日期格式化为字符串
+        String formattedDate = currentDate.format(formatter);
+
+        String message = String.format("今天%s上证指数是多少？", formattedDate);
+        String chat = chatAssistant.chat(message);
 
         System.out.println(chat);
     }
