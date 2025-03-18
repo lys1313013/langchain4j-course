8878f6057bfdfd4d156700da0dc5ef83be1ecdc1
lys1313013 <lys1313013@gmail.com>
2025-03-19 00:13:13 +0800

    feat: ollama增加多次对话demo

--- langchain4j-18-ollama/src/test/java/test/ChatTest.java
+++ langchain4j-18-ollama/src/test/java/test/ChatTest.java
@@ -1,6 +1,9 @@
 package test;
 
+import dev.langchain4j.data.message.AiMessage;
+import dev.langchain4j.data.message.UserMessage;
 import dev.langchain4j.model.chat.ChatLanguageModel;
+import dev.langchain4j.model.output.Response;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -17,9 +20,25 @@ public class ChatTest {
     private ChatLanguageModel chatLanguageModel;
 
     @Test
-    void contextLoads() {
+    void chat() {
         String generate = chatLanguageModel.generate("你好");
 
         System.out.println(generate);
+    }
+
+    /**
+     * 多次对话
+     */
+    @Test
+    void mulChat() {
+        UserMessage userMessage = UserMessage.from("你是谁");
+        Response<AiMessage> response = chatLanguageModel.generate(userMessage);
+        AiMessage aiMessage = response.content();
+        System.out.println(aiMessage.text());
+
+        UserMessage userMessage2 = UserMessage.from("请重复一次");
+
+        Response<AiMessage> response2 = chatLanguageModel.generate(userMessage, aiMessage, userMessage2);
+        System.out.println(response2.content().text());
     }
 }
